d21fefd387aad4999443f5e13f74df7c3f1d5364
Gábor Farkas <farkas.gz@gmail.com>
2023-09-21 12:28:01 +0200

    need to add form later

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
+import { useState } from "react";
 import "./App.css";
+import foodsJson from "./foods.json"
+import FoodBox from "./components/FoodBox";
 
 function App() {
+  const [foods, setFoods] = useState(foodsJson);
+
+  const deleteFood = (id) => {
+    setFoods(foods.filter(e => e.id !== id))
+  }
+
   return (
     <div className="App">
       <h1>LAB | React IronNutrition</h1>
+      {foods.map((food) => 
+        <FoodBox key={food.id} food={food} deleteItem={deleteFood}/>
+      )}
     </div>
   );
 }
--- src/components/FoodBox.jsx
+++ src/components/FoodBox.jsx
@@ -1,1 +1,21 @@
 // Your code here
+function FoodBox({food, deleteItem}) {
+    return (
+        <div>
+            <p>{food.name}</p>
+
+            <img src={food.image} style={{height: "150px"}}/>
+
+            <p>Calories: {food.calories}</p>
+            <p>{food.servings} Servings</p>
+
+            <p>
+                <b>Total Calories: {food.calories * food.servings} </b> kcal
+            </p>
+
+            <button onClick={() => deleteItem(food.id)}>Delete</button>
+        </div>
+    )
+}
+
+export default FoodBox;
